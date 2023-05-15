80106a33a4b87424c06c9a4c000d0e8893612aeb
Aaron Kirchhoff <askirchhoff@gmail.com>
2023-05-15 07:00:18 -0700

    div boxes

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import './styles.css';
 
 // rcc is the react snippet to get this boilerplate component started.
 export default class boxes extends Component {
   render() {
     return (
-      <div>boxes</div>
+      <div>
+        <div id='tribox1' class="triBox"></div>
+        <div id='tribox2' class="triBox"></div>
+        <div id='tribox3' class="triBox"></div>
+        <div class='singleBox'></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     )
   }
 }
